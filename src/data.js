a951402b470d07099339303135e2354515a96747
SanjuXepiar <sanjusarkar238@gmail.com>
2024-09-10 13:40:37 +0530

    data updated

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projectData = [
     id: "1",
     title: "React Practices",
     description:
-      "Some basic React Practices like counter, show password, password match, todo list, addcart.",
+      "Some basic React Practices like counter (in which the count state increases and decreases on clickekin the increment and decrement button respectively), show password ( the function used in the app dynamically changes the type attribute of the input tag) , password match (checks whether the password typed in both the inputs same), todo list (this app has a input tag on cicking additem button a new item is added to the list and in each list there is a edit button which can be used to update the list item a delete button to delete the list item and all the data in thr list is locally stored), etc",
     date: "2 July 2024",
     liveLink: "https://reactpractice01.netlify.app/",
     sourceLink: "https://github.com/SanjuXepiar/react-practice1",
@@ -11,7 +11,8 @@ export const projectData = [
   {
     id: "2",
     title: "Mobile Cart",
-    description: "Add items to your Cart.",
+    description:
+      "The Mobile Cart allows users to browse a selection of products and add items   to a virtual shopping cart.The app typically displays a product catalog, and users can interact with items by selecting quantities and viewing the total cost.",
     date: "8 July 2024",
     liveLink: "https://carts-app0.netlify.app",
     sourceLink: "https://github.com/SanjuXepiar/addcart",
@@ -19,26 +20,38 @@ export const projectData = [
   {
     id: "3",
     title: "Menubar",
-    description: "Toggle the Categories of food-items.",
+    description:
+      "Toggle the Categories of food-items. Menu items are grouped into categories. Users can click on a button to view items under a specific category, hiding the others.",
     date: "28 July 2024",
     liveLink: "https://menu-bar01.netlify.app",
     sourceLink: "https://github.com/SanjuXepiar/menu-bar",
   },
   {
     id: "4",
-    title: "Weather-App",
-    description: "Check Weather in your city.",
-    date: "2 August 2024",
-    liveLink: "https://weather-search01.netlify.app/",
-    sourceLink: "https://github.com/SanjuXepiar/weatherapp",
-  },
-  {
-    id: "5",
     title: "Typing-Game",
     description:
-      "Check the speed of your typing. The app separate the correct and wrong words in separate columns and also shows the number of correct words.",
+      "Check the speed of your typing. The app separate the correct and wrong words in separate columns and also displays the number of correct words.",
     date: "4 August 2024",
     liveLink: "https://typing-game-test.netlify.app",
     sourceLink: "https://github.com/SanjuXepiar/typing-game",
+  },
+  {
+    id: "5",
+    title: "BackRoad-App",
+    description:
+      "The Backroads App is a travel planning and discovery platform designed to help adventurers explore off-the-beaten-path destinations. Whether you're a solo traveler, a family, or a group of friends, the app allows users to find unique experiences, scenic routes, and hidden gems around the world.",
+    date: "31 August 2024",
+    liveLink: "https://backroads-01app.netlify.app",
+    sourceLink: "https://github.com/SanjuXepiar/backroads-app",
+  },
+
+  {
+    id: "6",
+    title: "Weather-App",
+    description:
+      "The Weather App provides weather information for various locations, helping users informed about weather conditions, forecasts, and other related data. It typically allows users to search for a city to display weather details",
+    date: "28 July 2024",
+    liveLink: "https://weather-search01.netlify.app",
+    sourceLink: "https://github.com/SanjuXepiar/weatherapp",
   },
 ];
