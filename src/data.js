200108102895b8762810cc98058c9e072ec35fa3
SanjuXepiar <sanjusarkar238@gmail.com>
2024-09-10 12:19:46 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projectData = [
     id: "1",
     title: "React Practices",
     description:
-      "Some basic React Practices like counter, show password, password match, todo list, addcart",
+      "Some basic React Practices like counter, show password, password match, todo list, addcart.",
     date: "2 July 2024",
     liveLink: "https://reactpractice01.netlify.app/",
     sourceLink: "https://github.com/SanjuXepiar/react-practice1",
@@ -11,7 +11,7 @@ export const projectData = [
   {
     id: "2",
     title: "Mobile Cart",
-    description: "Add items to your Cart",
+    description: "Add items to your Cart.",
     date: "8 July 2024",
     liveLink: "https://carts-app0.netlify.app",
     sourceLink: "https://github.com/SanjuXepiar/addcart",
@@ -19,7 +19,7 @@ export const projectData = [
   {
     id: "3",
     title: "Menubar",
-    description: "Toggle the Categories of food-items",
+    description: "Toggle the Categories of food-items.",
     date: "28 July 2024",
     liveLink: "https://menu-bar01.netlify.app",
     sourceLink: "https://github.com/SanjuXepiar/menu-bar",
@@ -27,7 +27,7 @@ export const projectData = [
   {
     id: "4",
     title: "Weather-App",
-    description: "Search Cities' Weather",
+    description: "Check Weather in your city.",
     date: "2 August 2024",
     liveLink: "https://weather-search01.netlify.app/",
     sourceLink: "https://github.com/SanjuXepiar/weatherapp",
